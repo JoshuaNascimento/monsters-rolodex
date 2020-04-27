dcfbaa1d1e5e41fde5594efedb4de293eba72dfc
JoshuaNascimento <joshua.l.nascimento@gmail.com>
2020-04-26 23:29:31 -0400

    Updates

---
+++ precache-manifest.2d3b27b2c22f93f5be9459f4b1a5e53d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79c64b84f3668cad9ef2f9b07ebd19c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8f27f3e96dff14764204",
+    "url": "/monsters-rolodex/static/css/main.1c31a2f1.chunk.css"
+  },
+  {
+    "revision": "d35afea4fb86a47ba0af",
+    "url": "/monsters-rolodex/static/js/2.bd8fbb1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bd8fbb1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f27f3e96dff14764204",
+    "url": "/monsters-rolodex/static/js/main.ff530817.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  }
+]);
